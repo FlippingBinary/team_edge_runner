dca89e83c6402bd09a546c33ab931874418e50bc
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-04-02 06:43:04 -0400

    chore: Merge `testbench samples - aayush` into `testbenches`

--- cadence_rtl/testbenches/line_buffer_tb.sv
+++ cadence_rtl/testbenches/line_buffer_tb.sv
@@ -7,15 +7,15 @@ module line_buffer_tb;
   logic rstN; 
   logic [7:0] i_data; 
   logic i_data_valid;
-  logic rd_data; 
+  logic rd_enable; 
   logic [23:0] o_data;
 
-  line_buffer lb1 (.clk(clk), .rstN(rstN), .i_data(i_data), .i_data_valid(i_data_valid), .rd_enable(rd_data), .o_data(o_data));
+  line_buffer lb1 (.clk(clk), .rstN(rstN), .i_data(i_data), .i_data_valid(i_data_valid), .rd_enable(rd_enable), .o_data(o_data));
 
   always #5 clk = ~clk;
 
   initial begin
-    $monitor("@ time=%0t clk=%0b rstN=%0b i_data=%0h i_data_valid=%0b rd_data=%0b o_data=%0h", $time, clk, rstN, i_data, i_data_valid, rd_data, o_data);
+    $monitor("@ time=%0t clk=%0b rstN=%0b i_data=%0h i_data_valid=%0b rd_enable=%0b o_data=%0h", $time, clk, rstN, i_data, i_data_valid, rd_enable, o_data);
   end
 
   initial begin
@@ -23,7 +23,7 @@ module line_buffer_tb;
     rstN = 0;
     i_data = 0;
     i_data_valid = 0;
-    rd_data = 0;
+    rd_enable = 0;
     
     #10;
     rstN = 1;
@@ -39,7 +39,7 @@ module line_buffer_tb;
     #10;
     
     // test for reading data from the buffer
-    rd_data = 1;
+    rd_enable = 1;
     for (int i = 0; i < IMAGE_WIDTH; i++) begin
       if (o_data != i) $error("Mismatch at index %0d: Expected = %0d Output = %0d", i, i, o_data); 
       #10;
